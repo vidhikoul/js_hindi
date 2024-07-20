eafb47473e7f08b1c058da320353e3241e5144b5
Vidhi Koul <130899675+vidhikoul@users.noreply.github.com>
2024-07-20 17:31:21 +0000

    datatypes

---
+++ 02_basics/datatypes.js
@@ -0,0 +1,14 @@
+"use strict";//treat all js code as newer version
+// alert(3+3)//this will show error as we are using node js not browser
+//number => 2 to power 53
+//bigint
+//string=""
+//boolean => true/false
+//null=>standalone value
+//undefined=>
+    //symbol=>unique
+//object
+console.log(typeof "vidhi");
+//string
+// type of null=>object
+// type of undefined=>undefined.
