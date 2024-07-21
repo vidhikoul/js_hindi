cd67a8be0a82eb3061e7209db0209005b9fad692
Vidhi Koul <130899675+vidhikoul@users.noreply.github.com>
2024-07-21 13:16:30 +0000

    dataype_summary

---
+++ 02_basics/datatypes_summary.js
@@ -0,0 +1,21 @@
+//primitive
+
+//7 types: string,Numbetr,Boolean,null,undefined,symbol,BigInt
+
+
+//reference (non primitive)
+
+//array,objects,functions
+
+const heros=["vidhi","mom","papa"];
+let myobj=
+{
+    name:"vidhi",
+    age:20,
+
+}
+const myFunction =function (){
+    console.log("hellooooo");
+
+}
+//null->typeof=>object*****************
